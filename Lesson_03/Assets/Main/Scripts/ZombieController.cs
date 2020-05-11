bd2d0fe8fb36b41241c0cef8d755fabf17ab4b2f
koyima <koyima@users.noreply.github.com>
2020-05-11 10:44:07 +0300

    moved files into folder

---
+++ Lesson_03/Assets/Main/Scripts/ZombieController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieController : MonoBehaviour
+{
+    NavMeshAgent thisAgent;
+    Animator thisAnimator;
+
+    public bool PlayerDetected = false;
+    public Transform Player;
+
+    float AttackDistance = 0.463f;
+    bool AttackCalled = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisAgent = GetComponent<NavMeshAgent>();
+        thisAnimator = GetComponent<Animator>();
+
+        PlayerDetected = false;
+        Player = GameObject.Find("Brian").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerDetected && !AttackCalled)
+        {  
+            if (Vector3.Distance(transform.position, Player.position) <= AttackDistance)
+            {
+                thisAnimator.SetFloat("Speed", 0);
+
+                if (AttackCalled == false)
+                {
+                    thisAnimator.SetTrigger("Attack");
+                    Player.GetComponent<PlayerController>().BeingAttacked();
+                    AttackCalled = true;
+                }                
+            }
+            else
+            {
+                thisAgent.SetDestination(Player.position);
+                thisAnimator.SetFloat("Speed", 1);
+            }            
+        }
+        else 
+        {
+            thisAnimator.SetFloat("Speed", 0);
+        }
+    }
+}
