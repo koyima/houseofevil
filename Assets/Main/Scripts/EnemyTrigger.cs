029095f921ff702168366cb468ffde9252ae4747
koyima <koyima@users.noreply.github.com>
2020-05-11 10:32:51 +0300

    First upload

---
+++ Assets/Main/Scripts/EnemyTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrigger : MonoBehaviour
+{
+    ZombieController thisZombieController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisZombieController = transform.root.GetComponent<ZombieController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            thisZombieController.PlayerDetected = true;
+        }
+    }
+}
